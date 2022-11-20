573865f891dbdfa83c8f07538c1423bad8b21627
Kirankumar Sripati <kirankumar@backbase.com>
2022-11-20 18:57:42 +0530

    Updates

---
+++ precache-manifest.3b97f9e625f44dfe369a002fd81cf559.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572b841fe7348692021c37ed174c21df",
+    "url": "/reactnd-myreads/index.html"
+  },
+  {
+    "revision": "aa0390784b6d55b10133",
+    "url": "/reactnd-myreads/static/css/main.76bab4d8.chunk.css"
+  },
+  {
+    "revision": "db41c6f922866bd2adf1",
+    "url": "/reactnd-myreads/static/js/2.025e45f5.chunk.js"
+  },
+  {
+    "revision": "c9c5f12326e730f6171c9e7086115877",
+    "url": "/reactnd-myreads/static/js/2.025e45f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0390784b6d55b10133",
+    "url": "/reactnd-myreads/static/js/main.63479625.chunk.js"
+  },
+  {
+    "revision": "fcb61e001ca4eb421646",
+    "url": "/reactnd-myreads/static/js/runtime-main.bdcdc55e.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reactnd-myreads/precache-manifest.3b97f9e625f44dfe369a002fd81cf559.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactnd-myreads/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
